cf7415a3618c09483e9c01e8bb2aa025623c6af7
JoMaPeTe <jmperezterrero@gmail.com>
2025-05-02 00:07:24 +0200

    resolucion add character dbz

---
+++ bases/src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,9 @@
+import {  Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-list',
+  templateUrl: './list.component.html',
+  standalone: false,
+  styleUrl: './list.component.css',
+})
+export class ListComponent { }
