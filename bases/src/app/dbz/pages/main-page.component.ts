78fe04ae2547c08579ab22e477b7e7bf5868f528
JoMaPeTe <jmperezterrero@gmail.com>
2025-05-07 12:33:49 +0200

    input recibir datos del padre

--- bases/src/app/dbz/pages/main-page.component.ts
+++ bases/src/app/dbz/pages/main-page.component.ts
@@ -1,8 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html',
   standalone: false,
 })
-export class MainPageComponent {}
+export class MainPageComponent {
+  public characters: Character[] = [
+    {
+      name: 'Goku',
+      power: 9500,
+    },
+    {
+      name: 'Krillin',
+      power: 1000,
+    },
+    {
+      name: 'Vegeta',
+      power: 7500,
+    },
+  ];
+}
